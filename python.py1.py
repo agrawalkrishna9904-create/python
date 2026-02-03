d69c1e7c9964229a3e5b970164cfdf518423bc90
Kishna G Agrawal <agrawalkrishna9904@gmail.com>
2026-02-03 19:22:24 +0530

    Add files via upload

---
+++ python.py1.py
@@ -0,0 +1,21 @@
+print(9) #print a number
+print("Hello World")
+msg="Hello World"
+print(msg)
+
+#Mathematical Calculation
+a=10
+b=6
+c=4
+print("Three numbers are 10 6 and 4")
+add=a+b+c
+print("Sum",add)
+sub=a-b-c
+print("Subtraction",sub)
+mul=a*b*c
+print("Multiplication",mul)
+div=a/b/c
+print("Division",div)
+
+#power of numberprint(2**3)
+
