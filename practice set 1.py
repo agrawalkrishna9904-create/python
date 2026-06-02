00b2a195fe184edf0aa0493062ad0d0aee844b49
Kishna G Agrawal <agrawalkrishna9904@gmail.com>
2026-06-02 19:47:32 +0530

    Add files via upload

---
+++ practice set 1.py
@@ -0,0 +1,24 @@
+#1] write a program to input 2 numbers and print their sum
+
+a= int(input("Enter first number:"))
+b= int(input("Enter second number:"))
+print("sum=",a+b)
+
+
+##2] WAP to input side of square and print its area
+side =int(input("Enter the side:"))
+print("Area=",side**2)
+
+
+###3]  WAP to input 2 floating points and print their average
+f1=float(input("First number:"))
+f2=float(input("second number:"))
+print("Average=", (f1+f2)/2)
+
+
+####4]WAP to input 2 int a and b , print true if a is greater then or equal to b if not print false
+a=int(input("value of a is:"))
+b=int(input("value of b is:"))
+print(a>=b)
+
+
