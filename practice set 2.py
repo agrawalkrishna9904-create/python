5d0838c357ec2e7525b656bf98baadacfba2d7bc
Kishna G Agrawal <agrawalkrishna9904@gmail.com>
2026-06-09 21:55:05 +0530

    Add files via upload

---
+++ practice set 2.py
@@ -0,0 +1,12 @@
+##1] WAP to input users first name and print its length
+name=input("Enter users name:")
+print(len(name))
+print("length of the name is:",len(name))
+
+
+##2] WAP to fint the occurence of $ in a string
+str="I am $ studying $ from $"
+print(str.count("$"))
+
+name=input("Enter some string:")
+print(name.count("$"))
